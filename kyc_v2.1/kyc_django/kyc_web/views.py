ca83cd111b66ccdaeff43c6d02ddf03415e57b71
Chathura Madhusanka <66965842+chathuramadusanka@users.noreply.github.com>
2021-03-24 17:08:44 +0530

    ginjar templating

--- kyc_v2.1/kyc_django/kyc_web/views.py
+++ kyc_v2.1/kyc_django/kyc_web/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h2>hello....... first one</h2>")
+    return render(request, "kyc_web/index.html")
 
 def addkyc(request):
     return HttpResponse("<h2> you can add kyc</h2>")
