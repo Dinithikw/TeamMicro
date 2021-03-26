0a4090c57c6e3fa3c909dfdad19b81151c440f02
Chathura Madhusanka <66965842+chathuramadusanka@users.noreply.github.com>
2021-03-26 10:45:38 +0530

    handling database

--- kyc_v2.1/kyc_django/kyc_web/views.py
+++ kyc_v2.1/kyc_django/kyc_web/views.py
@@ -4,7 +4,8 @@ from  .models import KycInfo
 # Create your views here.
 
 def index(request):
-    return render(request, "kyc_web/index.html")
+    all_kyc = KycInfo.objects.all()
+    return render(request, "kyc_web/index.html", {'kyc_name': all_kyc})
 
 def addkyc(request):
     return HttpResponse("<h2> you can add kyc</h2>")
@@ -21,4 +22,6 @@ def add_kyc_submit(request):
 
     kyc_info = KycInfo(name_init=name_init, full_name=full_name, company_name=company_name, address_full=address_full, email_add=email_add, phone_num=phone_num, long_text=long_text)
     kyc_info.save()
-    return render(request, "kyc_web/index.html")
+    all_kyc = KycInfo.objects.all()
+    return render(request, "kyc_web/index.html", {'kyc_name': all_kyc})
+    #return render(request, "kyc_web/index.html")
