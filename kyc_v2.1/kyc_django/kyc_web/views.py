d2191cff732d677d0fb565e72eba8788682b15d0
Chathura Madhusanka <66965842+chathuramadusanka@users.noreply.github.com>
2021-03-26 14:54:49 +0530

    modifying view.py to show proper message boxes if condition satisfies

--- kyc_v2.1/kyc_django/kyc_web/views.py
+++ kyc_v2.1/kyc_django/kyc_web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from  .models import KycInfo
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -20,8 +21,20 @@ def add_kyc_submit(request):
     phone_num = request.POST["phone_num"]
     long_text = request.POST["details_area"]
 
-    kyc_info = KycInfo(name_init=name_init, full_name=full_name, company_name=company_name, address_full=address_full, email_add=email_add, phone_num=phone_num, long_text=long_text)
-    kyc_info.save()
-    all_kyc = KycInfo.objects.all()
-    return render(request, "kyc_web/index.html", {'kyc_name': all_kyc})
+
+    if KycInfo.objects.filter(email_add=email_add).exists():
+        messages.warning(request, 'email already exists')
+        return redirect('index')
+
+    else:
+
+        kyc_info = KycInfo(name_init=name_init, full_name=full_name, company_name=company_name,
+                           address_full=address_full, email_add=email_add, phone_num=phone_num, long_text=long_text)
+        kyc_info.save()
+        messages.success(request, 'Successfully submitted')
+        return redirect('index')
+        all_kyc = KycInfo.objects.all()
+
+
+    return render(request, "index.html", {'kyc_name': all_kyc})
     #return render(request, "kyc_web/index.html")
