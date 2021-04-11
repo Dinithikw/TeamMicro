0c775a0dbc937ceb47f730af5311fec3cf156757
Chathura Madhusanka <66965842+chathuramadusanka@users.noreply.github.com>
2021-04-11 15:14:33 +0530

    customizing database

--- kyc_django/kyc_django/kyc_django/urls.py
+++ kyc_django/kyc_django/kyc_django/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from kyc.views import index
 from kyc.views import office, personal, account, insertkyc, insertkyc1, update, edit_val, update_data
